7f967abc2e00c3582ce18722de204c6c90c43abe
Adelto Oliveira Almeida <adtguitar@gmail.com>
2021-02-04 03:30:18 -0300

    Procurando Bug no codigo, e refatorando

--- components/Slider/index.js
+++ components/Slider/index.js
@@ -1,4 +1,4 @@
-function createStyle(css) {
+const createStyle = (css) => {
     const head = document.querySelector('head');
     const style = `
         <style>${css}</style>
@@ -9,21 +9,56 @@ function createStyle(css) {
 const Action = (css) => {
     createStyle(css);
     return (
-        `<li class="action" style="${css}"></li>`
+        `<li class="action"></li>`
     )
 };
+const action = Action `
+    .action {
+        position: absolute;
+        list-style: none;
+        width: calc( var(--line-height) * 2);
+        height: calc( var(--line-height) * 2);
+        background: var( --color-ball);
+        border-radius: 50%;
+        left: 5px;
+    }
+    .action.second {
+       left: 100px;
+    }
+    .action.third {
+        transform: translateX(140px);
+    }
+`
 
+function clearAction(action) {
+    action.classList.remove('second');
+    action.classList.remove('third');
+}
 
-function handleClick() {
-    console.log('vai');
+const handleClick = (event) => {
+    const {target} = event;
+    const allItems = document.querySelectorAll('.item');
+    const action = document.querySelector('.action');
+
+    allItems.forEach((item) =>{
+        item.classList.remove('active');
+    })
+    target.classList.add('active');
+
+    clearAction(action);
+    if (target.classList.contains('pos-2')) {
+        action.classList.add('second');
+    }
+    if (target.classList.contains('pos-3')) {
+        action.classList.add('third');
+    }
 }
 
 const Items = (css, className)  => {
     createStyle(css);
     return (
         `<li    class = "item" ${className} 
-                style = "${css}" 
-                onclick= "handleClick()">
+                onclick= "handleClick(event)">
         </li>`
     )
 };
@@ -35,47 +70,35 @@ const Slider = (css, content) => (
 const states = [true, false, false];
 
 function showItems (stateItems)  {
-  const itemsShow = stateItems.map(state =>{
+
+    const items = (pos)=> Items`
+        .item {    
+            position: relative;
+            list-style: none;
+            width: calc( var(--line-height) * 2);
+            height: calc( var(--line-height) * 2);
+            border-radius: 50%;
+            background-color: var(--color-bar);
+            cursor: pointer;
+        }
+        ${`pos-${pos}`};
+    `
+    const itemAcitve = (pos)=> Items `
+        .item.active {
+            transform: scale(1.8);
+        }
+ 
+     ${`active pos-${pos}`};
+    `
+    const itemsShow = stateItems.map((state, index) =>{
         if(state == true) {
-            return itemAcitve;
+            return itemAcitve(index + 1);
         }else {
-            return items;
+            return items(index + 1);
         }
     });
     return itemsShow.join('')
 };
-
-
-
-const action = Action `
-    .action {
-        position: absolute;
-        list-style: none;
-        width: calc( var(--line-height) * 3);
-        height: calc( var(--line-height) * 3);
-        background: var( --color-ball);
-        border-radius: 50%;
-        left: 5px;
-    }
-`
-const itemAcitve = Items `
-    .item {  
-        position: relative;
-        list-style: none;
-        width: calc( var(--line-height) * 4);
-        height: calc( var(--line-height) * 4);
-        border-radius: 50%;
-        background-color: var(--color-bar);
-     }
-`
-const items = Items`
-    position: relative;
-    list-style: none;
-    width: calc( var(--line-height) * 2);
-    height: calc( var(--line-height) * 2);
-    border-radius: 50%;
-    background-color: var(--color-bar);
-`
 
 const slider = Slider `
     position: relative;
